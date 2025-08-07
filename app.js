9daddc6beb2ea060d1d8037948fa9341fbbfc9fd
joaovitorfirme123 <joaovitorfirme560@gmail.com>
2025-08-07 20:30:43 -0300

    aprendendo rotas

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 const express = require('express');
 const app = express();
 
+app.get("/",function(req,res){
+    res.send("Seja Bem-vindo Ao Nosso Site")
+});
+
+app.get("/artigos/:id",function(req,res){
+    if(req.params.id == "1"){
+        res.send("1 - como criar aplicativos android e ios")
+    }
+    else if(req.params.id == "3"){
+        res.send("Como usar o node js")
+    }
+    else{
+        res.send("Nenhum artigo foi encontrado")
+    }
+})
+app.get("/contato",function(req,res){
+    res.send("Deixe a sua dúvida aqui.")
+})
+
 app.listen(8081,function(){
     console.log('o servidor está rodando');
 });
